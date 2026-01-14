4ff2b442f1112c10b4549ced9c9982d3b7cf8574
magicwebkraken <terryvenusdev520@gmail.com>
2026-01-15 07:07:50 +0900

    dfsfsfs

--- main-EDYBROLV.js
+++ main-EDYBROLV.js
@@ -55928,24 +55928,24 @@ var WP = (() => {
           {
             img: "assets/images/svg-icons/social-icons/telegram-black.svg",
             alt: "Telegram",
-            link: "https://t.me/kingmaxidoge",
+            link: "https://t.me/maxidogegroup",
           },
           {
             img: "assets/images/svg-icons/social-icons/twitter-black.svg",
             alt: "Twitter",
-            link: "https://x.com/kingMaxidoge",
+            link: "https://x.com/realmaxidoge",
           },
         ]),
         (this.socialMobile = [
           {
             img: "assets/images/svg-icons/social-icons/telegram.svg",
             alt: "Telegram",
-            link: "https://t.me/kingmaxidoge",
+            link: "https://t.me/maxidogegroup",
           },
           {
             img: "assets/images/svg-icons/social-icons/twitter.svg",
             alt: "Twitter",
-            link: "https://x.com/kingMaxidoge",
+            link: "https://x.com/realmaxidoge",
           },
         ]),
         this.subscriptions.push(
@@ -56439,7 +56439,7 @@ var qP = (() => {
             "mb-4",
             "gap-3",
           ],
-          ["href", "https://t.me/kingmaxidoge", "target", "_blank"],
+          ["href", "https://t.me/maxidogegroup", "target", "_blank"],
           [
             "src",
             "/assets/images/svg-icons/social-icons/telegram.svg",
@@ -56450,7 +56450,7 @@ var qP = (() => {
             1,
             "icon",
           ],
-          ["href", "https://x.com/kingMaxidoge", "target", "_blank"],
+          ["href", "https://x.com/realmaxidoge", "target", "_blank"],
           [
             "src",
             "/assets/images/svg-icons/social-icons/twitter.svg",
